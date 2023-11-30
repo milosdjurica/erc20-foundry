29c273a91298bd6d85dbe667ac071d148a7762fe
milosdjurica <milosdjurica13@gmail.com>
2023-11-30 14:47:41 +0100

    test balance and if allowance works

--- test/OZTokenTest.t.sol
+++ test/OZTokenTest.t.sol
@@ -13,8 +13,38 @@ contract OZTokenTest is Test {
     address lu = makeAddr("lu");
     address john = makeAddr("john");
 
+    uint public constant STARTING_BALANCE = 100 ether;
+
     function setUp() public {
         deployer = new DeployOZToken();
         ozToken = deployer.run();
+
+        vm.prank(msg.sender);
+        ozToken.transfer(bob, STARTING_BALANCE);
+        vm.prank(msg.sender);
+        ozToken.transfer(lu, STARTING_BALANCE);
+        vm.prank(msg.sender);
+        ozToken.transfer(john, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public {
+        assertEq(STARTING_BALANCE, ozToken.balanceOf(bob));
+    }
+
+    function testAllowancesWorks() public {
+        // transferFrom()
+        uint initialAllowance = 1000;
+
+        // Bob approves John to spend tokens on his behalf
+        vm.prank(bob);
+        ozToken.approve(john, initialAllowance);
+
+        uint transferAmount = 500;
+
+        vm.prank(john);
+        ozToken.transferFrom(bob, john, transferAmount);
+
+        assertEq(ozToken.balanceOf(john), STARTING_BALANCE + transferAmount);
+        assertEq(ozToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
     }
 }
